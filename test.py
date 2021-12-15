3d9b2f23424327e343864e0541a1681b4ac6196a
moxDroid Labs Inc <pritam.world@gmail.com>
2021-12-15 10:04:53 -0500

    app changes

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import unittest
 import app
 
+
 class TestHello(unittest.TestCase):
 
     def setUp(self):
@@ -24,7 +25,8 @@ class TestHello(unittest.TestCase):
         self.assertEqual(rv.status, '200 OK')
         self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
 
-#if __name__ == '__main__':
+
+# if __name__ == '__main__':
 #    unittest.main()
 
 # TEST_CASES
@@ -33,6 +35,7 @@ class TestHello(unittest.TestCase):
 if __name__ == '__main__':
     ############# Add these lines #############
     import xmlrunner
+
     runner = xmlrunner.XMLTestRunner(output='test-reports')
     unittest.main(testRunner=runner)
     ###########################################
